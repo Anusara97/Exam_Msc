3bfbe317020419d8a26aaf1bd65fd438fa0b5bb3
Anusara97 <anusara777rcm@gmail.com>
2024-10-31 07:20:31 +0530

    toString method is checked with an object

---
+++ Test.java
@@ -0,0 +1,7 @@
+public class Test {
+    public static void main(String[] args) {
+        Contacts c = new Contacts("abc@gmail.com", "12345", "Matara");
+
+        System.out.println(c);
+    }    
+}
