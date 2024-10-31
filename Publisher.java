710f865adbdd80a7def4b4e2b6b86a096475cfcf
Anusara97 <anusara777rcm@gmail.com>
2024-10-31 17:25:25 +0530

    Publisher class is created.

---
+++ Publisher.java
@@ -0,0 +1,23 @@
+public class Publisher {
+    private String publisherName;
+    private Contacts contact;
+
+    public Publisher(String publisherName, Contacts contact) {
+        setPublisherName(publisherName);
+        this.contact = contact;
+    }
+
+    public void setPublisherName(String publisherName) {
+        this.publisherName = publisherName;
+    }
+
+    public String getPublisherName() {
+        return publisherName;
+    }
+
+    @Override
+    public String toString() {
+        String msg = "The publisher is " + publisherName + " and its contact details are " + contact.toString();
+        return msg;
+    }
+}
