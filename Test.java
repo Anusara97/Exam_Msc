3ac21b7f09c758458531735905dd7db23f69176a
Anusara97 <anusara777rcm@gmail.com>
2024-11-01 15:00:09 +0530

    Project is tested upto now.

--- Test.java
+++ Test.java
@@ -1,7 +1,13 @@
 public class Test {
     public static void main(String[] args) {
-        Contacts c = new Contacts("abc@gmail.com", "12345", "Matara");
+        Contacts c1 = new Contacts("abc@gmail.com", "12345", "Matara");
+        Contacts c2 = new Contacts("godage@gmail.com", "00000", "Colombo");
+        Author[] a = new Author[3];
+        a[0] = new Author("Anusara Punchihewa", c1);
+        Publisher p1 = new Publisher("Godage Book Shop", c2);
 
-        System.out.println(c);
-    }    
+        Publication pub = new Publication("OOP-Testing", a, p1, 2024);
+
+        System.out.println("\n\n\n" + pub);
+    }
 }
