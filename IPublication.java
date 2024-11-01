edfa5cdfd99b25902ad6d0012762436b1248c289
Anusara97 <anusara777rcm@gmail.com>
2024-11-01 13:16:34 +0530

    Update IPublication.java
    Testing process

--- IPublication.java
+++ IPublication.java
@@ -1,20 +1,3 @@
 public interface IPublication {
-    public void setTitle(String title);
 
-    public void setAuthors(Author[] authors);
-
-    public void setPublisher(Publisher publisher);
-
-    public void setYear(int year);
-
-    public String getTitle();
-
-    public Author[] geAuthors();
-
-    public Publisher getPublisher();
-
-    public int getYear();
-
-    @Override
-    public String toString();
 }
